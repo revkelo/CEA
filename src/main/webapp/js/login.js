1f4c1e7e29d7d0b26c47386063e5dcbc0e61edae
Kevin Gonzalez <105946335+revkelo@users.noreply.github.com>
2024-05-20 22:56:45 -0500

    admin login

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,4 +1,23 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-	event.preventDefault(); // Evita el envío del formulario
-	window.location.href = 'admin.html'; // Redirige a cruds.html
-});
+document.addEventListener("DOMContentLoaded", function() {
+	// Obtener el formulario de inicio de sesión
+	var form = document.getElementById("loginForm");
+  
+	// Agregar un evento de envío al formulario
+	form.addEventListener("submit", function(event) {
+	  // Detener el envío del formulario
+	  event.preventDefault();
+  
+	  // Obtener los valores de usuario y contraseña
+	  var usuario = form.elements["username"].value;
+	  var contraseña = form.elements["password"].value;
+  
+	  // Verificar si el usuario y la contraseña son correctos
+	  if (usuario === "admin" && contraseña === "admin") {
+		// Redirigir a admin.html si las credenciales son correctas
+		window.location.href = "admin.html";
+	  } else {
+		// Mostrar una alerta si las credenciales son incorrectas
+		alert("Usuario o contraseña incorrectos");
+	  }
+	});
+  });
